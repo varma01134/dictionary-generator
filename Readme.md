70c3b34c9007080f154787d7b7e54612bc0f3adb
Théo <th-lepage@outlook.com>
2016-06-19 18:44:33 +0200

    Improved performance. Add a lots of features. Fixed a tons of bugs...

--- Readme.md
+++ Readme.md
@@ -1,3 +1,27 @@
-##dictionary-algorithm
+##dictionaryAlgorithm
 
-A simple dictionary generator in C++ to use with a bruteforce.
+A simple dictionary generator in C++. The goal was to make an efficient algorythm that can create a list of all possibilities from a dictionary. It also easy to compile (C++ 11) and it simply use [SimpleJSON](https://github.com/nbsdx/SimpleJSON) (header-only library).
+
+For example :
+A dictionary composed of {"A", "B", "C"} and the maximum word length set to 2 will give ->
+{"A", "AA", "AB", "AC", "B", "BA", "BB", "BC", "C", "CA", "CB", "CC"}.
+
+Configuration file :
+```json
+{
+  "dictionary" : ["A", "B", "C"],
+  "logLevel" : 2,
+  "output" : {
+    "directory" : "output/",
+    "enable" : true,
+    "wordsBeforeSplittingFiles" : 10000000
+  },
+  "secretWord" : "Super",
+  "statsEnabled" : true,
+  "wordsLength" : 2
+}
+```
+
+To do :
+- Speed improvements (use all cores, code)
+- Allow start at different positions (range system)
--- main.cpp
+++ main.cpp
@@ -1,76 +1,240 @@
+/*
+ * Name    : dictionaryAlgorithm
+ * Version : 1.0.0
+ * Author  : thdoteo
+ * URL     : https://github.com/thdoteo/dictionaryalgorithm
+ */
+
 #include <iostream>
-#include <math.h>
 #include <vector>
 #include <chrono>
+#include <fstream>
+#include <sys/stat.h>
+#include "json.hpp"
 
 using namespace std;
+using json::JSON;
 
-// Settings
-int wordsLength = 2;
-string outputFilePath = "dictionary.txt";
+bool debug = true;
+string configFileName = "config.json";
+string statsFileName = "stats.json";
+bool statsEnabled = true;
+vector<char> dictionary {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+int wordsLength = 5;
+string secretWord = "Super";
+bool outputEnabled = false;
+string outputDirectory = "output/";
+int wordsBeforeSplittingFiles = 10000000;
+int logLevel = 2;
 
-// Lists
-vector<char> characters {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '.', ',', '?', '!'};
-vector<string> words {};
-
-// Temp
-int totalWordsNumber = 0;
-string word = "";
+ofstream output;
+int filesCount = 0;
+long long int timeBeforeRunning = 0;
+long long int timeAfterRunning = 0;
+unsigned long long int totalWordsNumber = 0;
+unsigned long long int currentWordsNumber = 0;
 int lastProgress = 0;
+bool secretWordFound = false;
+string secretWordResult = "";
+string word = "";
 
-unsigned long getTimestamp() {
-    return std::chrono::duration_cast<std::chrono::milliseconds>
-            (std::chrono::system_clock::now().time_since_epoch()).count();
-}
-
-int getCurrentProgress() {
-    return trunc(words.size() * 100 / totalWordsNumber);
-}
-
-string setCharacterAt(string word, int index, char character) {
-    if(index + 1 > word.length()) {
-        return word.substr(0, index) + character + word.substr(index);
-    } else {
-        return word.substr(0, index) + character + word.substr(index + 1);
+int stop() {
+    if(debug) {
+        cout << endl << "Press any key to continue...";
+        cin.ignore();
+        exit(0);
     }
 }
 
-int iterateOverCharacters(int wordPosition) {
-    for(int i = 0; i < characters.size(); i++) {
+long long int getTimestamp() {
+    return chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now().time_since_epoch()).count();
+}
+
+inline bool fileExist(const string& name) {
+    struct stat buffer;
+    return (stat (name.c_str(), &buffer) == 0);
+}
+
+void updateStats() {
+    if(statsEnabled) {
+        JSON stats;
+        if(fileExist(statsFileName)) {
+            ifstream infile {statsFileName};
+            string statsContent((std::istreambuf_iterator<char>(infile)), std::istreambuf_iterator<char>());
+            stats = JSON::Load(statsContent);
+        }
+        string timestamp = to_string(getTimestamp());
+        stats[timestamp]["logLevel"] = logLevel;
+        stats[timestamp]["secretWord"] = secretWord;
+        stats[timestamp]["secretWordFound"] = secretWordFound;
+        for(int i = 0; i < dictionary.size(); i++) { stats[timestamp]["dictionary"].append(string(1, dictionary[i])); }
+        stats[timestamp]["wordsLength"] = wordsLength;
+        stats[timestamp]["wordsBeforeSplittingFiles"] = wordsBeforeSplittingFiles;
+        stats[timestamp]["totalWordsNumber"] = totalWordsNumber;
+        stats[timestamp]["time"] = (timeAfterRunning - timeBeforeRunning) / 1000;
+        stats[timestamp]["filesCount"] = filesCount;
+        if((timeAfterRunning - timeBeforeRunning) / 1000 != 0) { stats[timestamp]["words/s"] = totalWordsNumber / ((timeAfterRunning - timeBeforeRunning) / 1000); }
+        else { stats[timestamp]["words/s"] = totalWordsNumber; }
+        ofstream statsFile(statsFileName);
+        statsFile << stats;
+        statsFile.close();
+    }
+}
+
+void loadConfig() {
+    if(fileExist(configFileName)) { // Read config.json and update default settings
+        ifstream infile {configFileName};
+        string configContent((std::istreambuf_iterator<char>(infile)), std::istreambuf_iterator<char>());
+        JSON config = JSON::Load(configContent);
+
+        JSON dictionaryJSON = config["dictionary"];
+        dictionary.clear();
+        dictionary.shrink_to_fit();
+        for(int i = 0; i < dictionaryJSON.size(); i++) {
+            string character = dictionaryJSON[i].ToString();
+            dictionary.push_back(character[0]);
+        }
+        wordsLength = config["wordsLength"].ToInt();
+        secretWord = config["secretWord"].ToString();
+        outputEnabled = config["output"]["enable"].ToBool();
+        outputDirectory = config["output"]["directory"].ToString();
+        wordsBeforeSplittingFiles = config["output"]["wordsBeforeSplittingFiles"].ToInt();
+        logLevel = config["logLevel"].ToInt();
+        statsEnabled = config["statsEnabled"].ToBool();
+    } else { // Create config.json and fill it with default settings
+        JSON config;
+        for(int i = 0; i < dictionary.size(); i++) { config["dictionary"].append(string(1, dictionary[i])); }
+        config["wordsLength"] = wordsLength;
+        config["secretWord"] = secretWord;
+        config["output"]["enable"] = outputEnabled;
+        config["output"]["directory"] = outputDirectory;
+        config["output"]["wordsBeforeSplittingFiles"] = wordsBeforeSplittingFiles;
+        config["logLevel"] = logLevel;
+        config["statsEnabled"] = statsEnabled;
+        ofstream configFile(configFileName);
+        configFile << config;
+        configFile.close();
+    }
+}
+
+void sendToOutput(string word) {
+    if(outputEnabled) {
+        if(currentWordsNumber == 0 || currentWordsNumber % wordsBeforeSplittingFiles == 0) {
+            filesCount++;
+            output.close();
+            string path = outputDirectory + to_string(currentWordsNumber / wordsBeforeSplittingFiles) + ".words";
+            output.open(path);
+        } else {
+            output << "\n";
+        }
+        output << word;
+    }
+}
+
+int getCurrentProgress() {
+    return int(trunc(currentWordsNumber * 100 / totalWordsNumber));
+}
+
+string setCharacterAt(string word, unsigned int index, char character) {
+    return word.substr(0, index) + character;
+}
+
+void iterateOverCharacters(unsigned int wordPosition) {
+    for(int i = 0; i < dictionary.size(); i++) {
+        if(secretWordFound) { break; }
         if(wordPosition == 0) { word = ""; }
 
-        word = setCharacterAt(word, wordPosition, characters[i]);
-        cout << "\n" << wordPosition << ":" << characters[i] << "->" << word;
-        words.push_back(word);
+        word = setCharacterAt(word, wordPosition, dictionary[i]);
+        sendToOutput(word);
+        currentWordsNumber++;
 
-        int currentProgress = getCurrentProgress();
-        if(currentProgress != lastProgress) {
-            lastProgress = currentProgress;
-            cout << " - (" << currentProgress << "%)";
+        if(word == secretWord) {
+            secretWordFound = true;
+            secretWordResult = word;
+            break;
         }
 
+        int currentProgress = getCurrentProgress();
+
+        if(currentWordsNumber == 1 && (logLevel == 1 || logLevel == 2)) { cout << "\n"; }
+
+        if(logLevel == 1 && currentProgress != lastProgress) {
+            long long int timeToCompleteProcess =
+                    ((getTimestamp() - timeBeforeRunning) * (100 - currentProgress)) / currentProgress; //ms for 100%
+            long long int estimationEndTime = ((getTimestamp() + timeToCompleteProcess) - getTimestamp()) / 1000;
+            printf("\r");
+            cout <<
+            string(44 + wordsLength + to_string((timeBeforeRunning + timeToCompleteProcess) / 1000).size(), ' ');
+            printf("\r");
+            cout << "Working  (word: " << word << ",";
+            cout << string(wordsLength - word.size(), ' ') << " progress: " << currentProgress << "%)";
+        } else if(logLevel == 2 && currentProgress != lastProgress) {
+            long long int timeToCompleteProcess =
+                    ((getTimestamp() - timeBeforeRunning) * (100 - currentProgress)) / currentProgress; //ms for 100%
+            long long int estimationEndTime = ((getTimestamp() + timeToCompleteProcess) - getTimestamp()) / 1000;
+            printf("\r");
+            cout <<
+            string(44 + wordsLength + to_string((timeBeforeRunning + timeToCompleteProcess) / 1000).size(), ' ');
+            printf("\r");
+            cout << "Working  (word: " << word << ",";
+            cout << string(wordsLength - word.size(), ' ') << " progress: " << currentProgress << "%, ";
+            cout << "time left: " << estimationEndTime << "s)";
+        } else if(logLevel == 3 && currentProgress != lastProgress) {
+            cout << "\n";
+            long long int timeToCompleteProcess = ((getTimestamp() - timeBeforeRunning) * (100 - currentProgress)) / currentProgress; //ms for 100%
+            long long int estimationEndTime = ((getTimestamp() + timeToCompleteProcess) - getTimestamp()) / 1000;
+            cout << "Working  (" << wordPosition << ":" << dictionary[i] << "->" << word << ",";
+            cout << string(wordsLength - word.size(), ' ') << " progress: " << currentProgress << "%, ";
+            cout << "time left: " << estimationEndTime << "s)";
+        }
+
+        if(currentProgress != lastProgress) { lastProgress = currentProgress; }
+
         if((wordPosition + 1) < wordsLength) {
-            iterateOverCharacters(wordPosition + 1); //Change to ++ ?
+            iterateOverCharacters(wordPosition + 1);
         }
     }
 }
 
 int main() {
-    //How many possibilities will be generated ?
-    for(int i = 1 ; i <= wordsLength ; i++) {
-        totalWordsNumber += pow(characters.size(), i);
+    try {
+        loadConfig();
+
+        if(wordsLength == 0) {
+            // Fired when 0 or not int
+            throw string("wordsLength can't be equal to 0 and must be an integer.");
+        }
+
+        for(int i = 1; i <= wordsLength; i++) {
+            totalWordsNumber = totalWordsNumber + ceil(pow(dictionary.size(), i));
+        }
+        cout << "Starting (" << totalWordsNumber << " words are going to be created)";
+
+        if(outputEnabled) { mkdir(outputDirectory.c_str()); }
+        timeBeforeRunning = getTimestamp();
+        iterateOverCharacters(0);
+        timeAfterRunning = getTimestamp();
+        output.close();
+
+        cout << "\nDone     (";
+        cout << currentWordsNumber << " words in ";
+        cout << (timeAfterRunning - timeBeforeRunning) / 1000 << "s -> ";
+        if((timeAfterRunning - timeBeforeRunning) / 1000 != 0) { cout << totalWordsNumber / ((timeAfterRunning - timeBeforeRunning) / 1000) << " words/s, "; }
+        else { cout << totalWordsNumber << "/+ words/s, "; }
+        if (filesCount > 1) { cout << filesCount << " files)"; } else { cout << filesCount << " file)"; }
+
+        if(secretWordFound) {
+            cout << "\nRiddle   (I found the secret word, it's '" << secretWordResult <<"' :-O)";
+        } else {
+            cout << "\nRiddle   (I didn't found the secret word :-/)";
+        }
+
+        updateStats();
+
+        stop();
     }
-    cout << "At the end, we'll have " << totalWordsNumber << " words.";
-    cout << "\nStarting...\n";
-
-    //Run and keep a track of times
-    long timeBeforeRunning = getTimestamp();
-    iterateOverCharacters(0);
-    long timeAfterRunning = getTimestamp();
-
-    cout << "\n\nDone (" << timeAfterRunning - timeBeforeRunning << " ms, " << words.size() << " words, " << (words.size() * 1000) / timeAfterRunning - timeBeforeRunning << " words per second).";
-    cout << "\nHave a nice day.\n";
-
-    system("pause");
-    return 0;
+    catch (const string & message) {
+        cout << "Error : " << message;
+        stop();
+    }
 }
